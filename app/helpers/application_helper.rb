f25592f3964c0585927e89381a33a002af046bbb
410AngelaVu <andjela1993@gmail.com>
2021-01-16 15:34:02 +0100

    Fixing required changes requested by TSE

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-
-def if_user_logged_in
-   if user_signed_in?
-   	(link_to 'Log Out', destroy_user_session_path, method: :delete, data: { confirm: 'Are You Sure?' }, class:'btn pink') +
-   	(link_to current_user.name, user_path(current_user), class:'btn pink')
-   else
-   	( link_to 'Log In', new_user_session_path, class:'btn pink mx-3') +
-   	(link_to 'Sign Up', new_user_registration_path, class:'btn pink')
-   end
-end
+  def if_user_logged_in
+    if user_signed_in?
+      (link_to 'Log Out', destroy_user_session_path, method: :delete, data: { confirm: 'Are You Sure?' },
+                                                     class: 'btn pink') +
+        (link_to current_user.name, user_path(current_user), class: 'btn pink')
+    else
+      (link_to 'Log In', new_user_session_path, class: 'btn pink mx-3') +
+        (link_to 'Sign Up', new_user_registration_path, class: 'btn pink')
+    end
+  end
 end
