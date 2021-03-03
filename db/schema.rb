d8557f51c89570382183283e936f574940e2f27f
410AngelaVu <andjela1993@gmail.com>
2021-03-03 23:34:50 +0100

    Changed Ruby version in order to deploy app on Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_01_10_203207) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attendances", force: :cascade do |t|
-    t.integer "attendee_id"
-    t.integer "attended_event_id"
+    t.bigint "attendee_id"
+    t.bigint "attended_event_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id"
@@ -25,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_01_10_203207) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.string "title"
-    t.integer "creator_id"
+    t.bigint "creator_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
